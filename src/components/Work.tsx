17510597ec070ed68f3eae5c3af3efb641f914f7
sahupranjal83188-hash <sahupranjal83188@gmail.com>
2026-05-21 13:32:41 +0530

    Restore pinning and horizontal scroll effect to My Work section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,6 +1,51 @@
 import "./styles/Work.css";
 import WorkImage from "./WorkImage";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 const Work = () => {
+  useGSAP(() => {
+    let translateX: number = 0;
+
+    function setTranslateX() {
+      const box = document.getElementsByClassName("work-box");
+      const rectLeft = document
+        .querySelector(".work-container")!
+        .getBoundingClientRect().left;
+      const rect = box[0].getBoundingClientRect();
+      const parentWidth = box[0].parentElement!.getBoundingClientRect().width;
+      const padding: number =
+        parseInt(window.getComputedStyle(box[0]).padding) / 2;
+      translateX = rect.width * box.length - (rectLeft + parentWidth) + padding;
+    }
+
+    setTranslateX();
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".work-section",
+        start: "top top",
+        end: `+=${translateX}`, // Use actual scroll width
+        scrub: true,
+        pin: true,
+        id: "work",
+      },
+    });
+
+    timeline.to(".work-flex", {
+      x: -translateX,
+      ease: "none",
+    });
+
+    // Clean up (optional, good practice)
+    return () => {
+      timeline.kill();
+      ScrollTrigger.getById("work")?.kill();
+    };
+  }, []);
 
   return (
     <div className="work-section" id="work">
